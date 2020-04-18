c38d16fdd6a2b9b5d06380de24d99395f4a182c1
Heather Harper <heather.harper@student.onslow.school.nz>
2020-04-19 11:55:38 +1200

    Lollipop class uses constants to draw lollipop

--- Lollipop.java
+++ Lollipop.java
@@ -15,25 +15,31 @@ import java.awt.Color;
  * @version 19/04/2020
  */
 public class Lollipop {
+    // constants for lollipop dimensions
+    public static final double X = 300.0; // position from left
+    public static final double Y = 180.0; // position from above
+    public static final double SIZE = 80.0; // diameter of circle
+    public static final double STICK = 200.0; // length of stick
+    
+    /**
+     * Constructor initialises UI class
+     */
+    public Lollipop() {
+        UI.initialise();
+        UI.addButton("Quit", UI::quit);    // Comment out to checkstyle
+    }
 
     /**
      * Draw a red lollipop on a stick
      */
-    public Lollipop() {
-        UI.initialise();
-        //UI.addButton("Quit", UI::quit);    // Comment out to checkstyle
-    }
-
-    /**
-     * Draw a lollipop on the GUI
-     */
     private void drawLollipop() {
-        UI.setLineWidth(10);                //set width of stick
-        UI.drawLine(300, 200, 300, 400);    // draw line for stick 
-                                            //(300, 200) to (300, 400)        
-        UI.setLineWidth(1);              // set width of  line to 1
-        UI.setColor(Color.red);          // set color to red
-        UI.fillOval(260, 160, 80, 80);   // fill oval @(260, 160), 80x80        
+        // Draw stick
+        UI.setLineWidth(SIZE / 8.0);             
+        UI.drawLine(X, Y, X, Y + STICK); 
+        // Draw lollipop             
+        UI.setLineWidth(1);              
+        UI.setColor(Color.red);          
+        UI.fillOval(X - (SIZE / 2.0), Y - (SIZE / 2.0), SIZE, SIZE);         
     }
     
     
